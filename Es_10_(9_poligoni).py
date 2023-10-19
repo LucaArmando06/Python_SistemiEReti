00fe92f290f652bba1823394d1ab48840d3bf0d5
LucaArmando06 <luca.armando.342@itiscuneo.eu>
2023-10-19 18:36:59 +0200

    es turtle

--- Es_10_(9_poligoni).py
+++ Es_10_(9_poligoni).py
@@ -1,25 +1,41 @@
+import turtle
+
+def disegnaPoligono(t, num, lato):
+    #disegno poligono
+    gradi = 360 / num
+    t.begin_fill()#la turtle inizia il riempimento
+    for i in range(0,num):
+        t.forward(lato)
+        t.left(gradi)
+    t.end_fill()#la turtle termina il riempimento
+
+def posizionaTurtle(t, num, lato, x, y):
+    #posiziona la turtle prima di disegnare il poligono
+    t.penup()#la turtle si muove senza disegnare
+    if(num%3 == 0):
+        x = -100 
+        y = y + lato*4
+    else:
+        x = x + lato*4
+    t.goto(x, y)#la turtle si posiziona in x y
+    t.pendown()#la turtle si muove disegnando
+    return x,y
+
 def main():
-    import turtle
-
+    x= 0
+    y= -100
+    lung = int(input("inserire il numero del lato: "))
     finestra = turtle.Screen()
-    finestra.bgcolor("white")
+    tarta = turtle.Turtle()
+    tarta.shape("turtle")
+    tarta.color("red")#colore della linea
+    tarta.speed("slow")#velocità della turtle
 
-    lato = 20
-    spazio = 100
+    for num in range(3,12):
+        x, y=posizionaTurtle(tarta, num, lung, x, y)
+        disegnaPoligono(tarta, num, lung)
+        
+    finestra.mainloop() #gestione finestra
 
-    for i in range(9):
-        alice = turtle.Turtle()
-        alice.penup()
-        alice.goto((i % 3) * spazio, -(i // 3) * spazio)
-        alice.pendown()
-
-        angolo = 360 / i + 3
-
-        for _ in range(i + 3):
-            alice.forward(lato)
-            alice.right(angolo)
-
-    finestra.mainloop()
-    
 if __name__ == "__main__":
     main()
